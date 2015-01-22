e049227bea9c1e1711f78f2b60b745f218b7349f
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-01-22 12:47:54 +0200

    Add loop handling

--- screenshots.js
+++ screenshots.js
@@ -1,16 +1,43 @@
 var page = require('webpage').create();
 
-var pageHeight = 1024;
-var pageWidth = 768;
+/* Add pad method to numbers */
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) { s = "0" + s; }
+  return s;
+}
+
+/* Configuration */
+var pageHeight = 1920;
+var pageWidth = 1080;
+var waittime = 30 * 1000;
+
+/* ULRs to load */
+var urls = [
+  'http://eu-b.demo.qlik.com/QvAJAXZfc/opendoc.htm?document=qvdocs%2FItalian%20Schools%20Navigator.qvw&host=demo11&anonymous=true',
+  'http://eu-b.demo.qlik.com/QvAJAXZfc/opendoc.htm?document=qvdocs%2FItalian%20Schools%20Navigator.qvw&host=demo11&anonymous=true'
+];
 
 /* Set size of browser window */
 page.viewportSize = { width: pageWidth, height: pageHeight };
 page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
 
-/* Open page and render content */
-page.open('http://eu-b.demo.qlik.com/QvAJAXZfc/opendoc.htm?document=qvdocs%2FItalian%20Schools%20Navigator.qvw&host=demo11&anonymous=true', function() {
-	window.setTimeout(function () {
-		page.render('screenshot.png');
-        phantom.exit();
-	}, 40000);
-});
+var urlcount = urls.length;
+
+renderPage(urls.shift(), urlcount - urls.length);
+
+function renderPage(url, index) {
+  var filename = 'screenshot' + index.pad(2) + '.png';
+  console.log("Open " + url);
+  page.open(url, function() {
+      window.setTimeout(function () {
+          console.log("Write to " + filename);
+          page.render(filename);
+          if(urls.length > 0) {
+            renderPage(urls.shift(), urlcount - urls.length);
+          } else {
+            phantom.exit();
+          }
+      }, waittime);
+  });
+}
