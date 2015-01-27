f2690c6e1d98b1238657f85ccbf1517febb2e7c4
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-01-27 18:51:51 +0200

    Add support for detecting when page is loaded by looking for resources being loaded and do special handling for QlikView

--- screenshots.bat
+++ screenshots.bat
@@ -1,4 +1,4 @@
 echo off
 cd %~dp0
-rem %~dp0\phantomjs.exe %~dp0\screenshots.js %1
-%~dp0\TrifleJS.exe %~dp0\screenshots.js %1
+%~dp0\phantomjs.exe %~dp0\screenshots.js %1
+rem %~dp0\TrifleJS.exe %~dp0\screenshots.js %1
--- screenshots.js
+++ screenshots.js
@@ -11,14 +11,15 @@ Number.prototype.pad = function(size) {
 /* Configuration */
 var pageWidth = 1920;
 var pageHeight = 1080;
-var waittime = 3 * 1000;
+var waittime = 10 * 000;
 
 /* ULRs to load */
 var urls = [
-  'http://www.google.com',
+  'http://eu-b.demo.qlik.com/QvAJAXZfc/opendoc.htm?document=qvdocs%2FEpidemiology%20-Tycho.qvw&host=demo11&anonymous=true',
+  /* 'http://www.google.com',
   'http://www.google.com#q=javascript',
   'http://www.google.com#q=html',
-  'http://www.google.com#q=css'
+  'http://www.google.com#q=css' */
 ];
 
 /* Set size of browser window */
@@ -56,23 +57,74 @@ renderPage(urls.shift(), urlcount - urls.length);
 function renderPage(url, index) {
   var filename = 'screenshot' + index.pad(2) + '.png';
   console.log("Open " + url);
-  page.open(url, function() {
+  page.open(url, function (status) {
+    console.log("Loaded " + url);
+    // Retry if we failed to load the page
+    if (status !== 'success') {
+      renderPage(url, urlcount - urls.length);
+      return;
+    }
+
+    // Registre everything a resource is loaded so we make sure they a finished loading before taking the screenshot
+    var resources = {};
+    page.onResourceRequested = function (request) {
+      //console.log(request.id + ": " + request.url + ':' + request.stage);
+      resources[request.id] = request.stage;
+    };
+    page.onResourceReceived = function (response) {
+      //console.log(response.id + ": " + response.url + ':' + response.stage);
+      resources[response.id] = response.stage;
+    };
+
+    // Special handling for QlikView
+    page.onCallback = function (msg) {
+      resources['qlikviewpage'] = 'end';
+    };
+    var qlikviewpage = page.evaluate(function () {
+      if(typeof Qv === 'object') {
+        Qv.GetCurrentDocument().SetOnUpdateComplete(function() { window.callPhantom(); } );
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if(qlikviewpage && typeof trifle !== 'object') { // Disable on TrifleJS as it is not working
+      resources['qlikviewpage'] = 'start';
+    }
+    
+    // Make sure everything is loaded, before we try to do anything
+    var interval = window.setInterval(function () {
+      console.log("Check if everything is loaded");
+      // Run over all the resources and check they are loaded
+      for (var i in resources) {
+        if (resources[i] !== 'end') {
+          return false;
+        }
+      }
+      window.clearInterval(interval);
+
+      // Wait for waittime before taking screenshot
       window.setTimeout(function () {
-          console.log("Write " + filename);
-          page.render(filename);
-          slideshowslides.push(filename);
-          if(urls.length > 0) {
-            renderPage(urls.shift(), urlcount - urls.length);
-          } else {
-            indexhtml +=
-              "	</div>\n" +
-              " <script type=\"text/javascript\">var slideshowslides = " + JSON.stringify(slideshowslides) + ";</script>\n" +
-              "</body>\n" +
-              "</html>";
-            fs.remove("index.html");
-            fs.write("index.html", indexhtml, 'w');
-            phantom.exit();
-          }
+        // Render page and add to slideshowlist
+        console.log("Write to " + filename);
+        page.render(filename);
+        slideshowslides.push(filename);
+
+        // Take next url or finish with writing the index.html
+        if (urls.length > 0) {
+          renderPage(urls.shift(), urlcount - urls.length);
+        } else {
+          console.log("Write index.html");
+          indexhtml +=
+                  "	</div>\n" +
+                  " <script type=\"text/javascript\">var slideshowslides = " + JSON.stringify(slideshowslides) + ";</script>\n" +
+                  "</body>\n" +
+                  "</html>";
+          fs.remove("index.html");
+          fs.write("index.html", indexhtml, 'w');
+          phantom.exit();
+        }
       }, waittime);
+    }, 1000);
   });
 }
